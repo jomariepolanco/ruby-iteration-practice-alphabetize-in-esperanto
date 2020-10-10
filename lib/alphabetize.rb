edabf7c0b96a250ae561f522df374382d6f95c54
jomariepolanco <jomariepolanco@gmail.com>
2020-10-10 14:35:33 -0400

    done with comment

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,4 +3,5 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   # code here
   arr.sort_by {|ele| ele.split("").map {|char| ESPERANTO_ALPHABET.index(char)}}
+  #sort by each element and return new array where characters are alphabetized based on constant variable
 end
